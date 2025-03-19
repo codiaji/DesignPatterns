dac21db3c548b768c5a1e3ca304ea105f69d913f
codiaji <codiaji@gmail.com>
2025-03-19 15:24:05 +0000

    Facade pattern implemented

--- src/main/java/io/motassadderoon/Main.java
+++ src/main/java/io/motassadderoon/Main.java
@@ -2,29 +2,20 @@ package io.motassadderoon;
 
 public class Main {
     public static void main(String[] args) {
+        // Creating components
         Amplifier amp = new Amplifier();
         DVDPlayer dvd = new DVDPlayer();
         Projector projector = new Projector();
         Lights lights = new Lights();
         Screen screen = new Screen();
 
-        // Watching a movie manually
-        System.out.println("Getting ready to watch a movie...");
-        lights.dim(10);
-        screen.down();
-        projector.on();
-        projector.setInput(dvd);
-        amp.on();
-        amp.setVolume(5);
-        dvd.on();
-        dvd.play("Inception");
+        // Creating the Facade
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, dvd, projector, lights, screen);
 
-        // Stopping the movie manually
-        System.out.println("\nStopping the movie...");
-        dvd.off();
-        amp.off();
-        projector.off();
-        screen.up();
-        lights.on();
+        // Watching a movie
+        homeTheater.watchMovie("Inception");
+
+        // Stopping the movie
+        homeTheater.stopMovie();
     }
 }
