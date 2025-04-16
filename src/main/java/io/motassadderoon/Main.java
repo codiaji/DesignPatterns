0aef5bd287066ee0491b23735f6939b620eac7a0
codiaji <codiaji@gmail.com>
2025-04-16 18:42:28 +0100

    My Builder design pattern implementation

--- src/main/java/io/motassadderoon/Main.java
+++ src/main/java/io/motassadderoon/Main.java
@@ -1,20 +1,31 @@
+// io.motassadderoon.Main.java
 package io.motassadderoon;
 
 public class Main {
     public static void main(String[] args) {
         Computer computer1 = new ComputerBuilder()
-                                    .cpu("Intel i7")
+                .cpu("Intel i7")
                 .ram("16GB")
                 .storage("512GB SSD")
                 .graphicsCard("NVIDIA RTX 3070")
-                .HasWifi(true)
-                .hasBluetooth(true).build();
+                .hasWifi(true)
+                .hasBluetooth(true)
+                .build();
         System.out.println(computer1);
 
-        Computer computer2 = new Computer("AMD Ryzen 5", "8GB", "256GB SSD", null, false, false);
+        Computer computer2 = new ComputerBuilder()
+                .cpu("AMD Ryzen 5")
+                .ram("8GB")
+                .storage("256GB SSD")
+                .build();
         System.out.println(computer2);
 
-        Computer computer3 = new Computer("Intel i5", "8GB", "1TB HDD", "Integrated", true, false);
+        Computer computer3 = new ComputerBuilder()
+                .cpu("Intel i5")
+                .ram("8GB")
+                .storage("1TB HDD")
+                .hasWifi(true)
+                .build();
         System.out.println(computer3);
     }
 }
